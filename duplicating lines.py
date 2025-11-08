180898851a467396e2bcfb0725bac32c34b5920f
Spinoguy2122 <rigelanthonypalanca@gmail.com>
2025-11-08 16:13:34 +0800

    Add files via upload

---
+++ duplicating lines.py
@@ -0,0 +1,11 @@
+outputFile = open('my_file.txt', 'w')
+inputFile = open('New_file.txt', 'r')
+lines_seen_so_far = set()
+print("Eliminating duplicating lines")
+for line in inputFile:
+
+    if line not in lines_seen_so_far:
+        outputFile.write(line)
+        lines_seen_so_far.add(line)
+inputFile.close()
+outputFile.close()
